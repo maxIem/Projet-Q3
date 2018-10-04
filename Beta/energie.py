8c10e7590ee1becabb219c486ff02c4c54765d0c
Clausse Léopold <clausseleopold@gmail.com>
2018-10-04 18:25:05 +0200

    V1.1
    besoinMethaneAutotherme: fonctionnel

--- Beta/energie.py
+++ Beta/energie.py
@@ -13,6 +13,7 @@ dHWGS = -34000                  # Enthalpie de la réaction WGS en J/mol
 dHCH4 = -803000                 # Enthalpie de la combustion du CH4
 MmCH4 = 0.01604                 # Masse molaire du CH4 en kg/mol
 MmH2O = 0.018                   # Masse molaire du H2O en kg/mol
+MmO2 = 0.032                    # Masse molaire du O2 en kg/mol
 
 
 ###############################
@@ -31,18 +32,20 @@ def besoinEnergieClassique(tIn, tOut, n, k):                    # Energie necess
 
 # tIn  : temperature d'entree des gaz
 # tOut : temperature de sortie des gaz
-# n    : nombre de moles de CH4 en entree
-# k    : Rapport molaire H2O/CH4 dans l’alimentation du reacteur
+# n    : nombre de moles de CH4 en entree par seconde
 
-def besoinMethaneAutotherme(tIn, tOut, n, k):                    # Methane necessaire pour le SMR autotherme en kg
+def besoinMethaneAutotherme(tIn, tOut, n):                    # Methane necessaire pour le SMR autotherme en moles
+    k = 1.15                                                # Rapport molaire H2O/CH4 : fixe pour Autotherme
+    l = 0.6                                                 # Rapport molaire O2/CH4 : fixe pour Autotherme
+
     E1 = dHSMR * n                                          # Energie neccesaire a la reaction
-    E2 = CpGazP * (1223 - tIn) #* x                         # Energie necessaire pour augmenter la temperature du methane a bruler jusque 1223K
+    E2 = CpGazP * (1223 - tIn) * (2*l*MmO2 + 1*MmCH4) #*x   # Energie necessaire pour augmenter la temperature des gaz a bruler jusque 1223K
     E3 = CpGazP * (tOut - tIn) * (n*MmCH4 + n*k*MmH2O)      # Energie necessaire pour elever la temperature des gaz
-    E4 = dHCH4 #* x                                         # Energie degagee par la combustion du methane
+    E4 = dHCH4 #*x                                         # Energie degagee par la combustion du methane
 
     x = (-E1-E3) / (E2+E4)
     return (E1, E2, E3, E4, x)
 
 
 #print(besoinEnergieClassique(693, 1100, 1, 2.5))
-print(besoinMethaneAutotherme(300, 1300, 1, 2.5))
+print(besoinMethaneAutotherme(300, 1300, 10))
