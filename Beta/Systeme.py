0edf457b7072507edd9f313313f4a843dee926b5
Maxime Lemerle <maximelemerle@me.com>
2018-10-04 12:31:36 +0200

    Version 1.1
    -Embellissement du code
    -Stable\Systeme.py résous le système Vaporeformage

--- Beta/Systeme.py
+++ Beta/Systeme.py
@@ -2,7 +2,6 @@ import numpy
 from sympy import *
 from sympy.solvers import solve
 from sympy.solvers.solveset import nonlinsolve
-from scipy.optimize import fsolve
 from gekko import GEKKO
 ###############################
 #x = symbols('x')                #epsilon
@@ -15,21 +14,29 @@ TWGS = 480.0
 KSMR = 10**(-(11650/TSMR) + 13.076)
 KWGS = 10**((1910/TWGS) - 1.764)
 ###############################
+
+def equationVapo():
+    """
+    Résout le système non lineaire
+    """
+    m = GEKKO()
+    x = m.Var(value=1)
+    y = m.Var(value=1)
+    m.Equation(KSMR*((k+1)*flux + 2*x - y)**2 * (flux-x) * (k*flux - x - y) - ((x-y) * p**2 * (3*x + y)**3)==0)
+    m.Equation(KWGS*(x - y)*(k*flux - x - y) - y * (3*x+y)==0)
+    m.solve(disp=False)
+    print(x.value,y.value)
+
+equationVapo()
+
+
 """
+Ne trouve aucune solution
 def equation():
     eqSMR = KSMR*((k+1)*flux + 2*x - y)**2 * (flux-x) * (k*flux - x - y) - ((x-y) * p**2 * (3*x + y)**3)
     eqWGS = KWGS*(x - y)*(k*flux - x - y) - y * (3*x+y)
     eqSMR = simplify(eqSMR)
     eqWGS = simplify(eqWGS)
     return nonlinsolve([eqSMR,eqWGS], [x, y])
-
 print(equation())
 """
-
-m = GEKKO()
-x = m.Var(value=1)
-y = m.Var(value=1)
-m.Equation(KSMR*((k+1)*flux + 2*x - y)**2 * (flux-x) * (k*flux - x - y) - ((x-y) * p**2 * (3*x + y)**3)==0)
-m.Equation(KWGS*(x - y)*(k*flux - x - y) - y * (3*x+y)==0)
-m.solve(disp=False)
-print(x.value,y.value)
