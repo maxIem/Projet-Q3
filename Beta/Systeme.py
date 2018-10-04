fbe5db25485c880e819252a33be1687c8813b7a9
Maxime Lemerle <maximelemerle@me.com>
2018-10-04 12:15:32 +0200

    Version 1.1
    Système SMR et WGS résolu grâce à la méthode gekko

--- Beta/Systeme.py
+++ Beta/Systeme.py
@@ -2,22 +2,34 @@ import numpy
 from sympy import *
 from sympy.solvers import solve
 from sympy.solvers.solveset import nonlinsolve
-from sympy.solvers import solve
 from scipy.optimize import fsolve
+from gekko import GEKKO
 ###############################
-x = symbols('x')                #epsilon
-y = symbols('y')                #eta
+#x = symbols('x')                #epsilon
+#y = symbols('y')                #eta
 flux = 1.0
 p = 30.0
 k = 2.5
 TSMR = 1100.0
 TWGS = 480.0
-KSMR = 10**((-11660/TSMR) + 13.076)
+KSMR = 10**(-(11650/TSMR) + 13.076)
 KWGS = 10**((1910/TWGS) - 1.764)
 ###############################
+"""
 def equation():
-    eqSMR = KSMR*((k+1)*flux + 2*x - y)**2 * (flux-x) * (k*flux - x) - (x-y)*p**2*27*x**3
-    eqWGS = KWGS*(k*flux*(x - y) - x**2 + y**2) - y*(3*x + y)
-    return nonlinsolve([eqSMR, eqWGS], [x, y])
+    eqSMR = KSMR*((k+1)*flux + 2*x - y)**2 * (flux-x) * (k*flux - x - y) - ((x-y) * p**2 * (3*x + y)**3)
+    eqWGS = KWGS*(x - y)*(k*flux - x - y) - y * (3*x+y)
+    eqSMR = simplify(eqSMR)
+    eqWGS = simplify(eqWGS)
+    return nonlinsolve([eqSMR,eqWGS], [x, y])
 
 print(equation())
+"""
+
+m = GEKKO()
+x = m.Var(value=1)
+y = m.Var(value=1)
+m.Equation(KSMR*((k+1)*flux + 2*x - y)**2 * (flux-x) * (k*flux - x - y) - ((x-y) * p**2 * (3*x + y)**3)==0)
+m.Equation(KWGS*(x - y)*(k*flux - x - y) - y * (3*x+y)==0)
+m.solve(disp=False)
+print(x.value,y.value)
